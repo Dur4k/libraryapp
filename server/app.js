db02c323d2d80428cfbb0fe703d35293c505ab4a
Dur4k <55763863+Dur4k@users.noreply.github.com>
2021-05-30 17:32:13 +0100

    GraphQl

--- server/app.js
+++ server/app.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema');
+const express = require("express");
+const schema = require("./schema/schema");
+const { graphqlHTTP } = require("express-graphql");
 
 const app = express();
 
 // bind express with graphql
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
-    graphiql: true
-}));
+    graphiql: true,
+  })
+);
 
 app.listen(4000, () => {
-    console.log('now listening for requests on port 4000');
+  console.log("now listening for requests on port 4000");
 });
